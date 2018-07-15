411fdc9e6360078b0d2ef2a44ae70b24620bae00
Michael Bestas <mkbestas@lineageos.org>
2018-07-15 22:54:36 +0300

    SystemUI: Clock AM/PM style customization
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Change-Id: I7f27f135ae0d1e731c936792f655c115f107d20c
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: wulan17 <wulan17@komodos.id>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6651,6 +6651,17 @@ public final class Settings {
         public static final String ENABLE_TASKBAR = "enable_taskbar";
 
         /**
+         * Display style of AM/PM next to clock in status bar
+         * 0: No display (Gingerbread/ICS stock)
+         * 1: Small display (Froyo stock)
+         * 2: Normal display (Eclair stock)
+         * default: 0
+         * 
+         * @hide
+         */
+        public static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
+
+        /**
          * Force full screen for devices with cutout
          * @hide
          */
