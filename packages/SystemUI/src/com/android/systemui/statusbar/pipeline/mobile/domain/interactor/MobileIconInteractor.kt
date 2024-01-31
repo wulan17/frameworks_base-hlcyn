2418f363811e002d276f4dafe35ce2add7c7a3a0
Dhina17 <dhinalogu@gmail.com>
2024-01-31 16:47:49 +0000

    SystemUI: Forward port 'HD & wifi calling statusbar icons'
    Squashed:
    * SystemUI: Fix HD calling icon for RTL layout
    * SystemUI: sb: Fix VoWifi icon size
    * SystemUI: sb: Hide wifi airplane spacer when vowifi is available
    * SignalDrawable: Add support for Cut + Roaming back
    
    Adithya R <gh0strider.2k18.reborn@gmail.com>:
        HD icon shows for VoLTE/VoNR, and wifi calling for VoWiFi.
        Based on CLO's volte/vowifi icon implementation, reworked manually.
        Drawables taken from Nothing OS and manually tweaked.
    
    Dhina17 <dhinalogu@gmail.com>:
        Rewrite it for new status bar impl in Android 14.
        Move VoWifi icon to Wifi space.
        New VoWifi icon with SIM identification.
    
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Weijie Wang <quic_weijiew@quicinc.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: electimon <electimon@gmail.com>
    Co-authored-by: nift4 <nift4@protonmail.com>
    Original-Change-Id: I0190394690e9b38e0575ffb099be8a5ed2ae9d90
    Change-Id: Ib48b1295d060acf462b2f91e81cbb570fd8df42e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: wulan17 <wulan17@komodos.id>

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/domain/interactor/MobileIconInteractor.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/domain/interactor/MobileIconInteractor.kt
@@ -131,6 +131,18 @@ interface MobileIconInteractor {
 
     /** True when in carrier network change mode */
     val carrierNetworkChangeActive: StateFlow<Boolean>
+
+    /** True when VoLTE/VONR available */
+    val isMobileHd: StateFlow<Boolean>
+
+    /** See [MobileIconsInteractor.isMobileHdForceHidden]. */
+    val isMobileHdForceHidden: Flow<Boolean>
+
+    /** True when VoWifi available */
+    val isVoWifi: StateFlow<Boolean>
+
+    /** See [MobileIconsInteractor.isVoWifiForceHidden]. */
+    val isVoWifiForceHidden: Flow<Boolean>
 }
 
 /** Interactor for a single mobile connection. This connection _should_ have one subscription ID */
@@ -147,6 +159,8 @@ class MobileIconInteractorImpl(
     defaultMobileIconGroup: StateFlow<MobileIconGroup>,
     isDefaultConnectionFailed: StateFlow<Boolean>,
     override val isForceHidden: Flow<Boolean>,
+    override val isMobileHdForceHidden: Flow<Boolean>,
+    override val isVoWifiForceHidden: Flow<Boolean>,
     connectionRepository: MobileConnectionRepository,
     private val context: Context,
     val carrierIdOverrides: MobileIconCarrierIdOverrides = MobileIconCarrierIdOverridesImpl(),
@@ -383,4 +397,14 @@ class MobileIconInteractorImpl(
             .logDiffsForTable(tableLogBuffer, columnPrefix = "icon", initialValue = initial)
             .stateIn(scope, SharingStarted.WhileSubscribed(), initial)
     }
+
+    override val isMobileHd: StateFlow<Boolean> =
+        connectionRepository.imsState
+            .map { it.isHdVoiceCapable() }
+            .stateIn(scope, SharingStarted.WhileSubscribed(), false)
+
+    override val isVoWifi: StateFlow<Boolean> =
+        connectionRepository.imsState
+            .map { it.isVoWifiAvailable() }
+            .stateIn(scope, SharingStarted.WhileSubscribed(), false)
 }
